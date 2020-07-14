ef78f3771eb091512e1107e26ab078cc8998f429
Furkan Bayram <sefikfurkanbayram@gmail.com>
2020-07-15 01:57:09 +0300

    homepage layout was created.

--- ok/gulpfile.js
+++ ok/gulpfile.js
@@ -8,13 +8,13 @@ const cleanCSS = require('gulp-clean-css');
 sass.compiler = require('node-sass');
  
 gulp.task('sass', function () {
-  return gulp.src('./assets/scss/main.scss')
+  return gulp.src('./assets/scss/**/*.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('./assets/css'));
 });
  
 gulp.task('sass:watch', function () {
-  gulp.watch('./assets/scss/main.scss', gulp.series('sass', 'minify-css'));
+  gulp.watch('./assets/scss/**/*.scss', gulp.series('sass', 'minify-css'));
 });
 
  
